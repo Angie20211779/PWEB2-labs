5ec584923c9e39f1ada8dba3b3e8b276ab5ebed2
Angie Isasi <aisasi@unsa.edu.pe>
2024-06-08 16:42:18 -0500

    lab7

--- LAB7/projects/LAB7/calc/views.py
+++ LAB7/projects/LAB7/calc/views.py
@@ -2,4 +2,12 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(request):
-    return render(request, 'home.html', {'name':'Narvin'})
+    return render(request, "home.html", {"name":"Narvin"})
+
+def add(request):
+        
+    val1 = int(request.GET["num1"])
+    val2 = int(request.GET["num2"]) 
+    res = val1 + val2
+    
+    return render(request, "result.html", {'result': res})
